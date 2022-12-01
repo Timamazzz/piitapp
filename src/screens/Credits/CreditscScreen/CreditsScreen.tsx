57de13176d15da8cb63be59ed60faef52c49b1a2
Timama <76005360+Timamazzz@users.noreply.github.com>
2022-12-01 17:15:28 +0300

    CreditsScreen

--- src/screens/Credits/CreditscScreen/CreditsScreen.tsx
+++ src/screens/Credits/CreditscScreen/CreditsScreen.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../../constants/globalStyles';
+import styles from './styles';
+import CreditsList from '../../../components/Project/CreditsList/CreditsList';
 
 const CreditsScreen = () => {
   return (
-    <View>
-      <Text>CreditsScreen</Text>
+    <View style={globalStyles.flexOne}>
+      <View style={styles.header}>
+        <View>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '79%',
+              justifyContent: 'space-between',
+            }}>
+            <Text
+              style={[
+                globalStyles.grayText,
+                globalStyles.bigText,
+                globalStyles.boldText,
+              ]}>
+              Зачёты
+            </Text>
+            <Text style={[globalStyles.grayText, {fontSize: 24}]}>...</Text>
+          </View>
+          <Text style={[globalStyles.grayText]}>ИТ-999</Text>
+          <Text style={[globalStyles.grayText]}>Студент ФИО</Text>
+        </View>
+      </View>
+      <CreditsList />
     </View>
   );
 };
